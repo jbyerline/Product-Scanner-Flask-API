c7ad2b4a3ceb09fb7a90ab115937282f81865c2b
Jacob Byerline <jbyerline@gmail.com>
2022-06-13 22:03:15 -0700

    added lots of endpoints

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ app = Flask(__name__)
 CORS(app)
 
 driver = None
-# filePath = "/home/jbyerline/ProductScanner/products.json"
-filePath = "products.json"
+filePath = "/home/jbyerline/ProductScanner/products.json"
+# filePath = "products.json"
 
 
 def scan_for_products(products_to_scan):
@@ -83,8 +83,8 @@ def scan_for_products(products_to_scan):
 
 @app.before_first_request
 def init_app():
-    requests.get(
-        'https://text.byerline.me/send/6193419322/' + urllib.parse.quote("Product Scanner has started."))
+    # requests.get(
+    #     'https://text.byerline.me/send/6193419322/' + urllib.parse.quote("Product Scanner has started."))
     # Configure and Start Google Chrome
     global driver
     options = Options()
@@ -156,42 +156,198 @@ def update_product_state(id, state):
     return json.dumps(products_to_scan, indent=4)
 
 
+@app.route('/remove/<int:id>', methods=['GET'])
+def remove_product(id):
+    # Read in product data
+    f = open(filePath)
+    products_list = json.load(f)
+    f.close()
+
+    index_to_remove = None
+
+    # Iterate over all the products in the file
+    for i in range(len(products_list['products'])):
+        if products_list['products'][i]['id'] == id:
+            index_to_remove = i
+
+    if index_to_remove is None:
+        return "ID: " + str(id) + " not found", 400
+
+    products_list['products'][index_to_remove:index_to_remove + 1] = []
+
+    # Overwrite file with new state
+    f = open(filePath, "w")
+    f.write(json.dumps(products_list, indent=4))
+    f.close()
+
+    return "Product with ID: " + str(id) + " removed.", 200
+
+
 @app.route('/add', methods=['POST'])
 def add_product():
     # Parse POST Body
     try:
-        driver.get(request.json['url'])
-        page_content = driver.page_source
-        print("regex: ", request.json['regex'], "\nnegateRegex: ", request.json['negateRegex'])
+        # Verify all the necessary fields are in the body
+        brand = request.json['brand']
+        name = request.json['name']
+        image = request.json['image']
+        product_url = request.json['productURL']
+        scan_url = request.json['scanURL']
+        regex = request.json['regex']
+        negate_regex = request.json['negateRegex']
+        contact_numbers = request.json['contactNumbers']
+
+        # Read in current products
+        f = open(filePath)
+        product_list = json.load(f)
+        f.close()
+        print(product_list)
+
+        # Find id for new product
+        product_id = product_list["products"][len(product_list["products"])-1]["id"] + 1
+
+        # Set other default fields
+        is_found = False
+        last_found = "N/A"
+        number_of_trials = 0
+        is_enabled = True
+
+        # Form object
+        new_product = {
+            "id": product_id,
+            "brand": brand,
+            "name": name,
+            "image": image,
+            "productURL": product_url,
+            "scanURL": scan_url,
+            "regex": regex,
+            "negateRegex": negate_regex,
+            "isFound": is_found,
+            "lastFound": last_found,
+            "numberOfTrials": number_of_trials,
+            "isEnabled": is_enabled,
+            "contactNumbers": [contact_numbers]
+        }
+
+        # Add new product to array
+        product_list["products"].append(new_product)
+
+        # Overwrite file with new state
+        f = open(filePath, "w")
+        f.write(json.dumps(product_list, indent=4))
+        f.close()
+
+        return "Products Updated", 200
+
     except KeyError:
-        return "Missing one or more keys. {\"id\" \"brand\" \"name\" \"image\" \"productURL\" \"scanURL\" \"regex\" " \
-               "\"negateRegex\" \"isFound\" \"lastFound\" \"numberOfTrials\" \"isEnabled\" \"contactNumbers\"}", 400
+        return "Missing one or more keys. {\"brand\" \"name\" \"image\" \"productURL\" \"scanURL\" \"regex\" " \
+               "\"negateRegex\" \"contactNumbers\"}", 400
+
+
+@app.route('/addPhone', methods=['Patch'])
+def add_phone():
+    try:
+        # Verify all the necessary fields are in the body
+        id = request.json['id']
+        contact_number = request.json['contactNumber']
+
+        # Read in current products
+        f = open(filePath)
+        product_list = json.load(f)
+        f.close()
+        print(product_list)
+
+        # Iterate over all of the products in the file
+        for product in product_list['products']:
+            # Find the product with the ID to update
+            if product['id'] == id:
+                # Check if phone number already exists
+                if contact_number in product['contactNumbers']:
+                    return "Number already exists", 400
+                else:
+                    product['contactNumbers'].append(contact_number)
+
+        # Overwrite file with new state
+        f = open(filePath, "w")
+        f.write(json.dumps(product_list, indent=4))
+        f.close()
+
+        return "Products Updated", 200
+
+    except KeyError:
+        return "Missing one or more keys. {\"id\" \"contactNumber\"}", 400
+
+
+@app.route('/removePhone', methods=['Patch'])
+def remove_phone():
+    try:
+        # Verify all the necessary fields are in the body
+        id = request.json['id']
+        contact_number = request.json['contactNumber']
+
+        # Read in current products
+        f = open(filePath)
+        product_list = json.load(f)
+        f.close()
+        print(product_list)
+
+        product_index = None
+        phone_index_to_remove = None
+
+        # Iterate over all the products in the file
+        for i in range(len(product_list['products'])):
+            if product_list['products'][i]['id'] == id:
+                product_index = i
+                for j in range(len(product_list['products'][i]['contactNumbers'])):
+                    if product_list['products'][i]['contactNumbers'][j] == contact_number:
+                        phone_index_to_remove = j
+
+        if product_index is None:
+            return "ID: " + str(id) + " not found", 400
+        elif phone_index_to_remove is None:
+            return "Phone Number: " + str(contact_number) + " was not found", 400
+
+        product_list['products'][product_index]["contactNumbers"][phone_index_to_remove:phone_index_to_remove + 1] = []
+
+        # Overwrite file with new state
+        f = open(filePath, "w")
+        f.write(json.dumps(product_list, indent=4))
+        f.close()
+
+        return "Products Updated", 200
+
+    except KeyError:
+        return "Missing one or more keys. {\"id\" \"contactNumber\"}", 400
 
 
 @app.route('/loadPage', methods=['POST'])
-def create_row_in_gs():
-    if request.method == 'POST':
-        try:
-            driver.get(request.json['url'])
-            page_content = driver.page_source
-            print("regex: ", request.json['regex'], "\nnegateRegex: ", request.json['negateRegex'])
-            if request.json['negateRegex']:
-                if not re.search(request.json['regex'], page_content):
-                    regex_response = "Regex Match Found"
-                else:
-                    regex_response = "Regex Match Not Found"
-            if not request.json['negateRegex']:
-                if re.search(request.json['regex'], page_content):
-                    regex_response = "Regex Match Found"
-                else:
-                    regex_response = "Regex Match Not Found"
+def load_page():
+    try:
+        url = request.json['url']
+        negate_regex = request.json['negateRegex']
+        regex = request.json['regex']
+        regex_response = False
 
-            response = {"url": request.json['url'], "regex": request.json['regex'],
-                        "negateRegex": request.json['negateRegex'], "regexResponse": regex_response,
-                        "pageContent": page_content}
-            return response
-        except KeyError:
-            return "Key \"url\" or \"regex\" or \"negateRegex\" not provided", 400
+        driver.get(url)
+        page_content = driver.page_source
+
+        if negate_regex:
+            if not re.search(regex, page_content):
+                regex_response = True
+            else:
+                regex_response = False
+        if not negate_regex:
+            if re.search(regex, page_content):
+                regex_response = True
+            else:
+                regex_response = False
+
+        response = {"url": url, "regex": regex, "negateRegex": negate_regex, "regexMatchFound": regex_response, "pageContent": page_content}, 200
+
+        return response
+
+    except KeyError:
+        return "Missing one or more keys. {[}\"url\" or \"regex\" or \"negateRegex\"}", 400
 
 
 if __name__ == '__main__':
